ca6fcfc3e413e1d6619c0e11ee03d95135565e4e
Henley <105320424+Henngz@users.noreply.github.com>
2022-08-28 16:13:55 -0500

    Update search.php

--- final_project/search.php
+++ final_project/search.php
@@ -32,7 +32,7 @@
 <body>
 <nav class="navbar navbar-expand-lg bg-light" style="background-color: #e3f2fd;">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">DouBan</a>
+    <a class="navbar-brand" href="#">WhatToWatch</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
